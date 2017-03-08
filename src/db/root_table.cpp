7cf9489012d9e92bcbb24fc41e37daf0b94d5ac9
姜伟 <jiangwei@jiangweideMacBook-Air.local>
2017-03-08 23:31:39 +0800

    fix bug

--- src/db/root_table.cpp
+++ src/db/root_table.cpp
@@ -46,7 +46,7 @@ void RootTable::dump(const std::string& root_table_file){
 		exit(-1);
 	}
 	
-	size_t prtable_spec_flexible_size = sizeof(RootTableSpec) + sizeof(RootTableSpec) * (block_offset_list_.size() - 1);
+	size_t prtable_spec_flexible_size = sizeof(RootTableSpec) + sizeof(RootTableSpec::data_type) * (block_offset_list_.size() - 1);
 	RootTableSpec* prtable_spec = (RootTableSpec*)malloc(prtable_spec_flexible_size);
 	strcpy(prtable_spec->magic_num_,RootTable::ROOT_MAGIC_NUM);
 	prtable_spec->list_size_ = block_offset_list_.size();
@@ -58,6 +58,10 @@ void RootTable::dump(const std::string& root_table_file){
 		printf("write root table: %s failed !\n",root_table_file.c_str());
 		exit(-1);	
 	}	
+	
+	fprintf(stderr,"size of RooTableSpec : %lu\n",sizeof(RootTableSpec));
+	fprintf(stderr,"size of flexi_RooTableSpec : %zu\n",prtable_spec_flexible_size);
+
 	free(prtable_spec);	
 	close(fd);
 }
@@ -75,6 +79,7 @@ void RootTable::fast_init_by_dump_file(const std::string& root_table_file){
 		exit(-1);
 	}
 	auto len = file_st.st_size;
+	printf("mmap file len size  %lld !\n",len);
 	RootTableSpec* prtable_spec = (RootTableSpec*)mmap(nullptr,len,PROT_READ,MAP_SHARED,fd,0);
 	if(prtable_spec == nullptr || prtable_spec == (void*)-1){
 		printf("mmap file %s failed !\n",root_table_file.c_str());
--- test/root_table_test.cpp
+++ test/root_table_test.cpp
@@ -1,5 +1,5 @@
 #include "../src/db/root_table.h"
-
+#include <iostream>
 
 namespace test{
 
@@ -13,6 +13,12 @@ public:
 			rtable.block_offset_list_.push_back(i);
 		}
 		rtable.dump();	
+		
+		db::RootTable rtable2;
+		rtable2.fast_init_by_dump_file(rtable_file);
+		for(auto elem : rtable2.block_offset_list_){
+			std::cout << elem << std::endl;
+		}	
 	}
 };//class RootTableTest
 
